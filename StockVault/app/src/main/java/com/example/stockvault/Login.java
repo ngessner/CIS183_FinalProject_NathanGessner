d6ecc3175041834dd515c3466e0b41c084df52b8
Nathan Gessner <ngessner04@gmail.com>
2024-11-14 23:37:07 -0500

    two activities made

--- StockVault/app/src/main/java/com/example/stockvault/Login.java
+++ StockVault/app/src/main/java/com/example/stockvault/Login.java
@@ -4,11 +4,8 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity
+public class Login extends AppCompatActivity
 {
 
     @Override
@@ -16,6 +13,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
